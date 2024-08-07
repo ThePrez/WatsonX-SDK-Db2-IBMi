b0aaa334567699f8c896ec4347e6fe40eca31ea3
worksofliam <mrliamallan@live.co.uk>
2024-08-07 15:06:48 -0400

    Add the ability to configure region and API version
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/install.sql
+++ src/install.sql
@@ -1,13 +1,25 @@
 
 create schema watsonx;
+create or replace variable watsonx.region varchar(16) ccsid 1208 default 'us-south';
+create or replace variable watsonx.apiVersion varchar(10) ccsid 1208 default '2023-07-07';
 create or replace variable watsonx.apikey varchar(100) ccsid 1208 default '';
 create or replace variable watsonx.spaceid varchar(100) ccsid 1208 default '';
 create or replace variable watsonx.JobBearerToken varchar(10000) ccsid 1208 default null;
 create or replace variable watsonx.JobTokenExpires timestamp;
 
---
--- (possible) TODO: could have a version that uses a database table with row permissions, as mentioned earlier
---
+create or replace function watsonx.GetUrl(route varchar(1000))
+  returns varchar(256) ccsid 1208
+begin
+  declare finalUrl varchar(256) ccsid 1208;
+
+  set finalUrl = 'https://' concat watsonx.region concat '.ml.cloud.ibm.com/ml/v1' concat route;
+
+  if (watsonx.apiVersion is not null and watsonx.apiVersion != '') then
+    set finalUrl = finalUrl concat '?version=' concat watsonx.apiVersion;
+  end if;
+
+  return finalUrl;
+end;
 
 create or replace procedure watsonx.SetApiKeyForJob(apikey varchar(100))
   program type sub
